2a3995993ee90842c651058e12dbad7ad4424eaa
m10rten <qmaartenenzo@gmail.com>
2022-10-02 13:06:40 +0200

    refactor to class

--- src/lib/cm-api/config/index.ts
+++ src/lib/cm-api/config/index.ts
@@ -1,3 +1,65 @@
+/* eslint-disable no-console */
+import { PrismaClient } from "@prisma/client";
+const actions = {
+  create: "/",
+  read: "/:id",
+  update: "/:id",
+  delete: "/:id",
+};
+import { Router } from "express";
+import { getPrismaSchemas } from "../data";
+// create routes for all the controllers
+const router = Router();
+
 export default class JetvilCMS {
-  // ...
+  private client: PrismaClient;
+  constructor(client: PrismaClient | undefined = undefined) {
+    this.client = client;
+  }
+
+  public setClient = async (client: PrismaClient) => {
+    this.client = client;
+  };
+
+  private getMethod = (action: string) =>
+    action === "read" ? "get" : action === "create" ? "post" : action === "update" ? "put" : "delete";
+
+  public router = (client: PrismaClient | undefined = undefined) => {
+    // this.client ??= client;
+    if (!this.client && client) {
+      throw new Error("No client provided");
+    }
+    const prismaClient = this.client ?? client;
+    console.log("CLIENT:::", prismaClient);
+    const schemas = getPrismaSchemas(prismaClient);
+    console.log("SCHEMAS:::", schemas);
+
+    schemas.forEach((schemaKey: string) => {
+      // const controllerRoutes = schemas[schemaKey];
+      const routes: (string | ((_req: any, _res: any) => void))[][] = [];
+      Object.keys(actions).forEach((action) => {
+        const route = [
+          action,
+          "/" + schemaKey + (actions as any)[action],
+          (_req: any, _res: any) => {
+            return _res.send(`Hello ${schemaKey} ${action} ${_req.params.id ? `with id: ${_req.params.id}` : ""}`);
+          },
+        ];
+        console.log(route);
+        routes.push(route);
+      });
+
+      routes.push(["read", `/${schemaKey}s`, (_req: any, _res: any) => _res.send(`Hello ${schemaKey}s`)]);
+
+      routes.forEach((route) => {
+        console.info(`Adding route: ${this.getMethod(route[0] as any)} ${route[1]}`);
+        // console.info(`Route has schema: `, controllerRoutes.schema);
+        // Object.keys(controllerRoutes.schema).forEach((key) => {
+        //   console.info(`Schema has key: ${key} with type: ${controllerRoutes.schema[key]}`);
+        // });
+        router[this.getMethod(route[0] as string)](route[1] as string, route[2] as (_req: any, _res: any) => void);
+      });
+    });
+    return router;
+  };
 }
--- src/lib/cm-api/data/index.ts
+++ src/lib/cm-api/data/index.ts
@@ -1,18 +1,6 @@
 /* eslint-disable no-console */
 import { PrismaClient } from "@prisma/client";
 
-export const handlePrismaClient = async (client: PrismaClient) => {
-  const conn = await client.$connect();
-  console.log(conn);
-  // ... you will write your Prisma Client queries here
-  const schemas = Object.keys(client).filter((key) => false === key.startsWith("_") && false === key.startsWith("$"));
-  console.log(schemas);
-  const results = await Promise.all(
-    schemas.map(async (schema: string) => {
-      const result: Array<any> = await (client as Record<string, any>)[schema].findMany();
-      return result;
-    }),
-  );
-  console.log(results);
-  return client;
+export const getPrismaSchemas = (client: PrismaClient) => {
+  return Object.keys(client).filter((key) => false === key.startsWith("_") && false === key.startsWith("$"));
 };
