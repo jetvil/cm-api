d927a080661d589363cb772645b2f2a3b7aaabe4
m10rten <qmaartenenzo@gmail.com>
2022-10-06 17:56:33 +0200

    created verbose logging and filter on findMany by req.query key:key=value

--- src/lib/cm-api/routes/index.ts
+++ src/lib/cm-api/routes/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import { logger } from "../config/logger";
 import { getPrismaSchemas } from "../data";
 import createSchemaRouter from "./schemaRouter";
 import { actions, methodTypes, middlewareType } from "./types";
@@ -10,21 +11,30 @@ const createIndexRouter = ({
   methods = Object.keys(actions) as Array<methodTypes>,
   schemas = getPrismaSchemas(client),
   middleware = [],
+  verbose,
 }: {
   client: PrismaClient;
   methods?: Array<methodTypes>;
   schemas?: Array<string>;
   middleware?: middlewareType;
+  verbose: boolean;
 }) => {
   const router: Router = Router();
   schemas ??= getPrismaSchemas(client);
 
+  router.use((_req, _res, next) => {
+    _res.header("CM-Powered-By", "Jetvil");
+    return next();
+  });
+
+  logger.info("Creating Schema routers...", verbose);
   schemas.forEach((schemaKey: string) => {
     const subRouter = createSchemaRouter({
-      client: client,
+      client,
       schema: schemaKey,
-      methods: methods,
-      middleware: middleware,
+      methods,
+      middleware,
+      verbose,
     });
     router.use(subRouter);
   });
